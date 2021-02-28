600df964823fd79bbaf30ad272165adc9cf1934f
nacolo118 <71738107+nacolo118@users.noreply.github.com>
2021-02-28 18:55:28 -0500

    Update main.js
    update function to execute with event listener

--- main.js
+++ main.js
@@ -1,37 +1,52 @@
-function updateMpValues(){
-  const mpList = [10579]
+$( "#update-info" ).submit(function( event ){
+    var inputMpList = $('#list-of-MPID').val()
+    console.log(inputMpList)
 
-  const mpValue1 = 'CORE'
-  const mpValue2= ''
-  const mpValue3= ''
-
-  const accountSID = 'IRhnL3pEPfNx1356456sFtTh3P5vqLh3o1'
-  const authToken = '_9Fc.nGeJbyAHRPGMFxLhUsX5zSstZdA'
-
-  for (i = 0; i < mpList.length; i++){
-
-    const url = `https://${accountSID}:${authToken}@api.impact.com/Advertisers/${accountSID}/MediaPartners/${mpList[i]}?MPValue1=${mpValue1}&MPValue2=${mpValue2}&MPValue3=${mpValue3}`
-
-    console.log(url)
-
-
-    const xhr = new XMLHttpRequest
-
-    xhr.responseType = 'json'
-    xhr.withCredentials = true;
-
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        console.log(this.responseText);
+    var mpArray = inputMpList.split(',')
+    console.log(mpArray)
+  
+    const mpValue1 = $('#MP-Value-1').val()
+    console.log(mpValue1)
+    const mpValue2= $('#MP-Value-2').val()
+    console.log(mpValue2)
+    const mpValue3= $('#MP-Value-3').val()
+    console.log(mpValue3)
+  
+    const accountSID = $('#SID').val()
+    console.log(accountSID)
+    //'IRhnL3pEPfNx1356456sFtTh3P5vqLh3o1'
+    const authToken = $('#Auth').val()
+    console.log(authToken)
+    //'_9Fc.nGeJbyAHRPGMFxLhUsX5zSstZdA'
+  
+    for (i = 0; i < mpArray.length; i++){
+  
+      const url = `https://${accountSID}:${authToken}@api.impact.com/Advertisers/${accountSID}/MediaPartners/${mpArray[i]}?MPValue1=${mpValue1}&MPValue2=${mpValue2}&MPValue3=${mpValue3}`
+  
+      console.log(url)
+  
+  
+      const xhr = new XMLHttpRequest
+  
+      xhr.responseType = 'json'
+      xhr.withCredentials = true;
+  
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          console.log(this.responseText);
+        }
+  
+        xhr.open('PUT', url);
+        xhr.send()
       }
 
-      xhr.open('PUT', url);
-      xhr.send()
-    }
     
-  }
-  
-console.log('updateMPValue ran')
-}
+      
+    }
 
-updateMpValues();
+    event.preventDefault();
+    console.log(`The Form was submitted and ${mpArray.length} Partner's values have been changed. Please spot check the UI to confirm changes`)
+    
+});
+
+
